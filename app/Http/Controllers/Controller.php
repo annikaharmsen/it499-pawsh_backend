8a2cb2bcdf1a060e30adc1b2feaf4b588983a565
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-06-05 14:40:08 +0200

    create controller response methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
-    //
+    function sendResponse(string $message, Array $data): JsonResponse {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ], 200);
+    }
+
+    function sendError(string $message, int $status): JsonResponse {
+        return response()->json([
+            'success' => false,
+            'message' => $message
+        ], $status);
+    }
 }
