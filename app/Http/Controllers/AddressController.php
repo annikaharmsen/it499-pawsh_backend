91c556455f0b03026f1d7bcbd13524d6abe06aa4
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-06-11 10:53:36 +0200

    address endpoints implemented

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\AddressResource;
 use App\Models\Address;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -11,34 +12,66 @@ use Illuminate\Validation\ValidationException;
 
 class AddressController extends Controller
 {
+    private $storeRules = [
+        'house_num' => 'required | integer | gt:0',
+        'apt_num' => 'integer | gt:0',
+        'street' => 'required | string | max:120',
+        'city' => 'required | string | max:120',
+        'state' => 'required | string | max:120',
+        'country' => 'required | string | max:120',
+        'name' => 'required | string | max:120',
+        'user_id' => 'nullable | exists:users,id'
+    ];
+
+    private $updateRules = [
+        'house_num' => 'nullable | integer | gt:0',
+        'apt_num' => 'nullable | integer | gt:0',
+        'street' => 'nullable | string | max:120',
+        'city' => 'nullable | string | max:120',
+        'state' => 'nullable | string | max:120',
+        'country' => 'nullable | string | max:120',
+        'name' => 'nullable | string | max:120',
+        'user_id' => 'nullable | exists:users,id'
+    ];
+
+    public function respondWithOne(String $message, Address $address): JsonResponse {
+        return parent::sendResponse($message, ['address' => new AddressResource($address)]);
+    }
+
+    public function respondWithMany(String $message, mixed $addresses): JsonResponse {
+        return parent::sendResponse($message, ['addresses' => AddressResource::collection($addresses)]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $addresses = Auth::user()->addresses;
+
+        return $this->respondWithMany('User\'s addresses retreived successfully', $addresses);
+    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        try {
-            $input = $request->validate([
-                'house_num' => 'required | integer | gt:0',
-                'apt_num' => 'integer | gt:0',
-                'street' => 'required | string | max:120',
-                'city' => 'required | string | max:120',
-                'state' => 'required | string | max:120',
-                'country' => 'required | string | max:120',
-                'name' => 'required | string | max:120',
-                'user_id' => 'nullable | exists:users,id'
-            ]);
-        } catch (ValidationException $e) {
-            return $this->sendError('Validation Error.', Response::HTTP_BAD_REQUEST);
-        }
-
-        if ($input['user_id'] != null && $input['user_id'] != Auth::id()) {
-            return $this->sendError('Not authorized to create address for this user.', Response::HTTP_UNAUTHORIZED);
-        }
+        $input = $this->validateOrError($request, $this->storeRules);
 
         $address = new Address($input);
+        $address['user_id'] = Auth::id();
+        $address->save();
 
-        return $this->sendResponse('Address saved successfully.', ['address' => new AddressResource($address)]);
+        return $this->respondWithOne('Address saved successfully.', $address);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Address $address)
+    {
+        return $this->respondWithOne('Address retreived successfully.', $address);
     }
 
     /**
@@ -46,7 +79,15 @@ class AddressController extends Controller
      */
     public function update(Request $request, Address $address)
     {
-        //
+        $input = $this->validateOrError($request, $this->updateRules);
+
+        if ($address->user_id !== Auth::id()) {
+            return $this->sendError('Address not found for this user.', Response::HTTP_NOT_FOUND);
+        }
+
+        $address->update($request->all());
+
+        return $this->respondWithOne('Address updated successfully.', $address);
     }
 
     /**
@@ -54,6 +95,12 @@ class AddressController extends Controller
      */
     public function destroy(Address $address)
     {
-        //
+        if ($address->user_id !== Auth::id()) {
+            return $this->sendError('Address not found for this user.', Response::HTTP_NOT_FOUND);
+        }
+
+        $address->delete();
+
+        return $this->sendResponse('Address deleted successfully.');
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
 abstract class Controller
 {
-    function sendResponse(string $message, Array $data): JsonResponse {
+    private Array $storeRules = [];
+    private Array $updateRules = [];
+
+    function sendResponse(string $message, Array $data = []): JsonResponse {
         return response()->json([
             'success' => true,
             'message' => $message,
             'data' => $data
-        ], 200);
+        ], Response::HTTP_OK);
     }
 
     function sendError(string $message, int $status): JsonResponse {
@@ -19,5 +26,13 @@ abstract class Controller
             'success' => false,
             'message' => $message
         ], $status);
+    }
+
+    function validateOrError(Request $request, Array $rules) {
+        try {
+            return $request->validate($rules);
+        } catch (ValidationException $e) {
+            return $this->sendError('Validation Error.', Response::HTTP_BAD_REQUEST);
+        }
     }
 }
