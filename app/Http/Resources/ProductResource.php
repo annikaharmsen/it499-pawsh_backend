26efcfb90ae4a0e3ab9343533b39f8b9a966b4fb
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-06-11 11:00:59 +0200

    product resource implemented

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,6 +14,12 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'price' => $this->price,
+            'inventory' => $this->inventory
+        ];
     }
 }
