08ba616a55a036d2776e6a9b618546ed743df388
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-06-13 18:31:34 +0200

    Implement order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'date',
+        'status',
+        'user_id',
+        'address_id'
+    ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function address(): BelongsTo {
+        return $this->belongsTo(Address::class);
+    }
+
+    public function items(): HasMany {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function payments(): BelongsTo {
+        return $this->belongsTo(Payment::class);
+    }
 }
